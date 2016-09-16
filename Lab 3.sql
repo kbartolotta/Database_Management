42988cd85309dbe17cc8bf99467801a1677b2423
kbartolotta <katherine.bartolotta1@marist.edu>
2016-09-16 18:21:37 -0400

    This is the third lab for database management.
    Began to test various SQL queries on the CAP3 database.

---
+++ Lab 3.sql
@@ -0,0 +1,43 @@
+﻿/*Katie Bartolotta
+Lab 3
+9/16/16*/
+
+select ordnum, totalUSD
+From orders;
+
+Select name, city
+from agents
+where name = 'Smith';
+
+select pid, name, priceUSD
+from products
+Where quantity > 201000;
+
+select name, city
+from customers
+where city = 'Duluth';
+
+select name
+from agents
+where city != 'New York' 
+	AND city != 'Duluth';
+
+select *
+from products
+where city != 'Dallas'
+	AND city != 'Duluth'
+	AND priceUSD >= 1;
+
+select *
+from orders
+where mon = 'feb'
+	OR mon = 'mar';
+
+select *
+from orders
+where mon = 'feb'
+	and totalUSD >= 600;
+
+select *
+from orders
+where cid = 'C005';
